1f41fffa04fab84427202606260ddb02da3f6ebe
Jose Teotonio <teo.nett@icloud.com>
2020-02-24 10:50:17 -0300

    Ajuste no layout e formato das paginas

---
+++ NetCoreBookStores/Migrations/20200224015819_Cadastro_Produtos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCoreBookStores.Migrations
+{
+    public partial class Cadastro_Produtos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
